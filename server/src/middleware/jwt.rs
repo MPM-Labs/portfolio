3be6fb295119fd7367f10d7610c836b89c49cfa7
MrProgrammerMan <69722297+MrProgrammerMan@users.noreply.github.com>
2026-05-10 19:43:44 +0200

    feat(tracing): add logging to jwt middleware

--- server/src/middleware/jwt.rs
+++ server/src/middleware/jwt.rs
@@ -5,6 +5,7 @@ use axum::{
 };
 use axum_extra::extract::CookieJar;
 use jsonwebtoken::{Validation, decode, errors::ErrorKind as jwtErrorKind};
+use tracing::{Level, event, instrument};
 
 use crate::{
     auth::{
@@ -15,6 +16,7 @@ use crate::{
     state::AppState,
 };
 
+#[instrument(skip_all)]
 pub async fn jwt_validation(
     State(state): State<AppState>,
     jar: CookieJar,
@@ -23,22 +25,41 @@ pub async fn jwt_validation(
 ) -> Result<Response, AppError> {
     let token_str = match jar.get("jwt") {
         Some(t) => t.value(),
-        None => return Ok(Redirect::to("/login").into_response()),
+        None => {
+            event!(
+                Level::DEBUG,
+                "No jwt cookie present, redirecting to /login"
+            );
+            return Ok(Redirect::to("/login").into_response());
+        }
     };
+
+    event!(Level::DEBUG, "JWT cookie present");
 
     let token_result = decode::<Claims>(token_str, &state.jwt_decode, &Validation::default());
 
     let token = match token_result {
         Ok(token) => token,
         Err(e) => match e.kind() {
-            jwtErrorKind::ExpiredSignature => return Ok(Redirect::to("/auth/refresh").into_response()),
-            _ => return Err(AppError::AuthError(AuthError::JWTError(e))),
+            jwtErrorKind::ExpiredSignature => {
+                event!(Level::DEBUG, "jwt expired, redirecting to /auth/refresh");
+                return Ok(Redirect::to("/auth/refresh").into_response());
+            }
+            _ => {
+                event!(Level::ERROR, error = ?e, "JWT error");
+                return Err(AppError::AuthError(AuthError::JWTError(e)));
+            }
         },
     };
+
+    event!(Level::DEBUG, role = ?token.claims.role, "JWT valid");
 
     match token.claims.role {
         Role::Superuser => Ok(next.run(req).await),
         #[allow(unreachable_patterns)]
-        _ => Err(AppError::AuthError(AuthError::Unauthorized)),
+        r => {
+            event!(Level::WARN, role = ?r, "Unauthorized role");
+            Err(AppError::AuthError(AuthError::Unauthorized))
+        }
     }
 }
