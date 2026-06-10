21be0e8f97dccdb9728241abd73205a58ee94428
MrProgrammerMan <69722297+MrProgrammerMan@users.noreply.github.com>
2026-06-10 09:42:29 +0200

    feat(model): user model for storing permissions

---
+++ server/src/models/user.rs
@@ -0,0 +1,17 @@
+#[derive(sqlx::Type)]
+#[sqlx(type_name = "role")]
+#[sqlx(rename_all = "lowercase")]
+pub enum Role {
+    Superuser,
+    Admin,
+    Contributor,
+}
+
+#[derive(sqlx::Type)]
+pub struct Email(String);
+
+#[derive(sqlx::Type)]
+pub struct User {
+    role: Role,
+    email: Email
+}
